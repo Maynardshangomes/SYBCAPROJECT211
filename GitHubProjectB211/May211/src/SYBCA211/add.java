bb9d9a80ed759129ba62bd80cf4a327a2d4d9b4d
furrynick6 <rbca22232.rcca@gmail.com>
2024-01-20 09:49:29 +0530

    add

---
+++ GitHubProjectB211/May211/src/SYBCA211/add.java
@@ -0,0 +1,15 @@
+package SYBCA211;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1=5;
+		int num2=7;
+		int num= num1+num2;
+		System.out.println("answer" +num);
+		
+	}
+
+}
